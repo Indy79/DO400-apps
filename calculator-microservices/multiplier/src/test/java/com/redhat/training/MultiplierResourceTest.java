075a40220ea813bea261470d80ce8f18542d2f66
indy79 <jeanbaptiste.joffre@gmail.com>
2023-05-14 04:06:40 -0400

    adding TU on multiplier resources"

--- calculator-microservices/multiplier/src/test/java/com/redhat/training/MultiplierResourceTest.java
+++ calculator-microservices/multiplier/src/test/java/com/redhat/training/MultiplierResourceTest.java
@@ -1,13 +1,22 @@
 package com.redhat.training;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 import com.redhat.training.service.SolverService;
 
+import org.jboss.resteasy.client.exception.ResteasyWebApplicationException;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class MultiplierResourceTest {
-    
+
     SolverService solverService;
     MultiplierResource multiplierResource;
 
@@ -16,4 +25,46 @@ public class MultiplierResourceTest {
         solverService = mock(SolverService.class);
         multiplierResource = new MultiplierResource(solverService);
     }
+
+    @Test
+    public void givenTwoPositiveValuesMustMultiplyAndReturnAnAnswer() {
+        // Given
+        when(solverService.solve("2")).thenReturn(Float.valueOf("2"));
+        when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+
+        // When
+        Float result = multiplierResource.multiply("2", "3");
+
+        // Then
+        assertEquals(6.0f, result);
+    }
+
+    @Test
+    public void givenOnePositiveValueAndOneNegativeValueMustMultiplyAndReturnAnAnswer() {
+        // Given
+        when(solverService.solve("-2")).thenReturn(Float.valueOf("-2"));
+        when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+
+        // When
+        Float result = multiplierResource.multiply("-2", "3");
+
+        // Then
+        assertEquals(-6.0f, result);
+    }
+
+    @Test
+    public void givenOnePositiveValueAndOneWrongValueMustMultiplyAndReturnAnError() {
+        // Given
+        WebApplicationException cause = new WebApplicationException("Unkown error", Response.Status.BAD_REQUEST);
+        when(solverService.solve("b")).thenThrow(new ResteasyWebApplicationException(cause));
+        when(solverService.solve("3")).thenReturn(Float.valueOf("3"));
+
+        // Then
+        assertThrows(ResteasyWebApplicationException.class, () -> {
+            // When
+            Float result = multiplierResource.multiply("b", "3");
+        });
+    }
+
+
 }
