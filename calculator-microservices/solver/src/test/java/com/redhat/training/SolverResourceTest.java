c4dddaab9f6d1cad3fc8e3ead1aec456b570040a
indy79 <jeanbaptiste.joffre@gmail.com>
2023-05-14 04:23:00 -0400

    add functionnal testing

--- calculator-microservices/solver/src/test/java/com/redhat/training/SolverResourceTest.java
+++ calculator-microservices/solver/src/test/java/com/redhat/training/SolverResourceTest.java
@@ -1,10 +1,43 @@
 package com.redhat.training;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.Test;
+
 import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
 
 @QuarkusTest
 @TestHTTPEndpoint(SolverResource.class)
 public class SolverResourceTest {
 
+    @Test
+    public void multiplyPositivInteger() {
+        RestAssured.given()
+                .when()
+                .get("2*3")
+                .then()
+                .statusCode(200)
+                .body(CoreMatchers.is("6.0"));
+    }
+
+
+    @Test
+    public void addPositivInteger() {
+        RestAssured.given()
+                .when()
+                .get("2+3")
+                .then()
+                .statusCode(200)
+                .body(CoreMatchers.is("5.0"));
+    }
+
+    @Test
+    public void addBadInteger() {
+        RestAssured.given()
+                .when()
+                .get("b+3")
+                .then()
+                .statusCode(400);
+    }
 }
